9044eb5311f58debd5544d10308aa071254aa70b
Mark <fqchenxin@gmail.com>
2020-12-02 23:44:10 +1100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  //extends: ["plugin:vue/essential", "@vue/prettier"],
+  rules: {
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+  parserOptions: {
+    "parser": "babel-eslint",
+    "ecmaVersion": "2015",
+    "sourceType": "module"
+  }
+};
